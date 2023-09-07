32f5f82b90f03f0f0577eadffb2313fd3d69ae18
Jorge Tinoco <volkney@Jorges-MacBook-Air.local>
2023-09-07 17:36:37 -0500

    added css loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
-
+import printMe from './print';
+import './style.css';
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
+ 
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+   element.classList.add('hello');
+ 
     return element;
   }
   
