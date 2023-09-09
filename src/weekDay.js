f1baad4787ce3716be77f1240fca6e88c6577b1c
Jorge Tinoco <volkney@Jorges-MacBook-Air-2.local>
2023-09-09 18:57:54 -0500

    added git ignore file to project

--- src/weekDay.js
+++ src/weekDay.js
@@ -2,6 +2,6 @@ export function getWeekDay(date) {
     let days =  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     let d = new Date(date);
     let dayName = days[d.getDay()];
-    console.log(dayName);
+    //console.log(dayName);
     return dayName;
 }
